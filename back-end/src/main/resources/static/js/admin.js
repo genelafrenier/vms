52f871daef1b27035aeec8a2bff7a134460eb983
Jordan Dawson <jtdawson333@gmail.com>
2023-11-22 21:17:53 -0800

    first draft of create event form

--- back-end/src/main/resources/static/js/admin.js
+++ back-end/src/main/resources/static/js/admin.js
@@ -18,7 +18,7 @@ function close_popup(){
     document.getElementById("popup").style.display = "none";
 }
 
-function ValidationTesting(){
+function validation_testing(){
 
     //title input error checking
     if(document.create_event.event_title.value==""){
@@ -28,11 +28,74 @@ function ValidationTesting(){
 
 
     //date input error checking
-    if(document.create_event.event_date.value==""){
+    else if(document.create_event.event_date.value==""){
         document.getElementById("date_error").innerHTML="Please Select a Date for your Event";
         return false;
     }
 
 
-    //
+    //location input error checking
+    else if(document.create_event.event_location.value==""){
+        document.getElementById("location_error").innerHTML="Please Enter a Location for your Event";
+        return false;
+    }
+
+
+    //time input error checking
+    else if(document.create_event.event_time.value==""){
+        document.getElementById("time_error").innerHTML="Please Select a Time for your Event";
+        return false;
+    }
+
+
+    //description input error checking
+    else if(document.create_event.event_description.value==""){
+        document.getElementById("description_error").innerHTML=="Please Enter a Description for your Event";
+        return false;
+    }
+
+
+    //call submit_form()
+    else {
+        //pull validated data into database
+        // *** printing to console to check data is going through *** //
+        document.getElementById("event_title");
+        console.log(event_title.value);
+
+        document.getElementById("event_date");
+        console.log(event_date.value);
+
+        document.getElementById("event_location");
+        console.log(event_location.value);
+
+        document.getElementById("event_time");
+        console.log(event_time.value);
+
+        document.getElementById("event_description");
+        console.log(event_description.value);
+
+        create_event();
+    }
+
+}
+
+//creates event and sends you back to dashboard
+function create_event(){
+    var title = document.getElementById("event_title").value;
+    var date = document.getElementById("event_date").value;
+    var location = document.getElementById("event_location").value;
+    var time = document.getElementById("event_time").value;
+    var description = document.getElementById("event_description").value;
+
+    var data = {
+        event_title: title,
+        event_date: date,
+        event_location: location,
+        event_time: time,
+        event_description: description
+    };
+    console.log(JSON.stringify(data));
+    //make an AJAX request to the event endpoint
+
+    
 }
