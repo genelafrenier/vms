1af9885b019eb65ef591373b5327ded6245b4072
Jordan Dawson <jtdawson333@gmail.com>
2023-11-25 15:39:35 -0800

    view profile popup frontend functional

---
+++ front-end/Admin Dash/event element/volunteer elements/pending element/pending_script.js
@@ -0,0 +1,10 @@
+
+
+// *** View Profile Popup *** //
+function view_profile(){
+    document.getElementById("profile_popup").style.display = "block";
+}
+
+function close_profile(){
+    document.getElementById("profile_popup").style.display = "none";
+}
