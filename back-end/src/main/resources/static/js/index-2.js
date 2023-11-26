1de0840636788100b6fcaec9d36a7ca07b3ebbb4
Phdinsleeping <144873707+Phdinsleeping@users.noreply.github.com>
2023-11-25 21:13:50 -0800

    Add files via upload

---
+++ back-end/src/main/resources/static/js/index-2.js
@@ -0,0 +1,12 @@
+function listview() {
+    document.getElementById("grid").style.display = "none";
+    document.getElementById("list").style.display = "grid";
+    document.getElementById("buttonGrid").style.backgroundColor="white";
+
+}
+
+function gridview() {
+    document.getElementById("list").style.display = "none";
+    document.getElementById("grid").style.display = "grid";
+
+}
